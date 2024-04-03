5160ff94d46ad90d7201b1e4224e371db05cbb33
Erfan Bafandeh <156314538+Erfan-Bafandeh@users.noreply.github.com>
2024-04-03 21:28:47 +0330

    Create __init__.py

---
+++ goodip/__init__.py
@@ -0,0 +1,9 @@
+from .ip import ip
+
+__title__: str = 'goodip'
+__version__: str = '0.0.1'
+__license__: str = 'MIT license'
+__author__: str = 'Erfan Bafandeh'
+__url__: str = 'https://github.com/Erfan-Bafandeh/goodip'
+__author_email__: str = 'user.enbh@gmail.com'
+__description__: str = 'you can get your ip address and get its information.'
